7752239f11ed271504044ec63e2fcf97f2481300
nguyenduy99 <nguyentienduy1104@gmail.com>
2022-02-21 23:33:39 +0700

    z

---
+++ Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseRegisterSystem.Models
+{
+    public partial class Enrollment
+    {
+        [Display(Name = "Mã lớp")]
+        [Required]
+        public long ClassId { get; set; }
+        
+        [Display(Name = "Mã số sinh viên")]
+        [Required]
+        public string StudentId { get; set; } = null!;
+
+        [DataType(DataType.Date)]
+        public DateTime? CreateAt { get; set; }
+
+        public virtual Class Class { get; set; } = null!;
+        public virtual Student Student { get; set; } = null!;
+    }
+}
